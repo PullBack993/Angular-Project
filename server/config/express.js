cc82e978795e5ab5c8479e3d679c992525fe7de9
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-03-31 18:12:09 +0200

    cors

--- server/config/express.js
+++ server/config/express.js
@@ -1,29 +1,28 @@
-const config = require('./cors')
-const cors = require("cors");
+// const config = require('./cors')
 const express = require("express");
+const cors = require("cors");
 const cookiParser = require("cookie-parser");
 const { authJwt } = require("../helpers/guards");
 const errorHandler = require("../helpers/error-handler");
 // const cors = require("./cors")
 
 module.exports = (app) => {
-  app.use(
-    cors(
-      // origin: config.origin,
-      // credentials: true,
-    )
-  );
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      return res.status(200).json({});
-    }
-    next();
-  })
+  app.use(cors());
+  // origin: config.origin,
+  // credentials: true,
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
+  //     res.setHeader('Access-Control-Allow-Credentials', true);
+  //     return res.status(200).json({});
+  //   }
+  //   next();
+  // })
   app.use(express.json());
+   
+
   app.use(cookiParser());
   app.use(authJwt());
   app.use(errorHandler);
