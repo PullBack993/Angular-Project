0bec66f0f3fb32c5f3b1fc31342a07aa8cc212ae
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-03-31 21:01:40 +0200

    Finaly setup for cors(errors)

--- server/config/express.js
+++ server/config/express.js
@@ -1,23 +1,11 @@
-// const config = require('./cors')
 const express = require("express");
 const cors = require("cors");
 const cookiParser = require("cookie-parser");
 const { authJwt } = require("../helpers/guards");
 const errorHandler = require("../helpers/error-handler");
-// const cors = require("./cors")
 
 module.exports = (app) => {
   app.use("*", cors());
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-  });
   app.use(express.json());
   app.use(cookiParser());
   app.use(authJwt());
