31bce7a366e323c534aeeff4ca6151f330b9642d
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-03-31 18:18:18 +0200

    cors again

--- server/config/express.js
+++ server/config/express.js
@@ -10,16 +10,16 @@ module.exports = (app) => {
   app.use(cors());
   // origin: config.origin,
   // credentials: true,
-  // app.use((req, res, next) => {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
-  //   if (req.method === 'OPTIONS') {
-  //     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
-  //     res.setHeader('Access-Control-Allow-Credentials', true);
-  //     return res.status(200).json({});
-  //   }
-  //   next();
-  // })
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      return res.status(200).json({});
+    }
+    next();
+  })
   app.use(express.json());
    
 
