c39cf5384ffcdb01c40048a84eeed20a2196e7a3
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-03-31 20:05:08 +0200

    add

--- server/config/express.js
+++ server/config/express.js
@@ -7,19 +7,19 @@ const errorHandler = require("../helpers/error-handler");
 // const cors = require("./cors")
 
 module.exports = (app) => {
-  // app.use(cors());
+  app.use(cors());
   // origin: config.origin,
   // credentials: true,
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      return res.status(200).json({});
-    }
-    next();
-  })
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
+  //     res.setHeader('Access-Control-Allow-Credentials', true);
+  //     return res.status(200).json({});
+  //   }
+  //   next();
+  // })
   app.use(express.json());
    
 
