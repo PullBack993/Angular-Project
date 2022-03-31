eaf4eb77a4217fca247dec0caabc992ac61995d2
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-03-31 20:48:15 +0200

    add

--- server/config/express.js
+++ server/config/express.js
@@ -7,22 +7,18 @@ const errorHandler = require("../helpers/error-handler");
 // const cors = require("./cors")
 
 module.exports = (app) => {
-  app.use(cors());
-
-  // origin: config.origin,
-  // credentials: true,
-  // app.use((req, res, next) => {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
-  //   if (req.method === 'OPTIONS') {
-  //     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
-  //     res.setHeader('Access-Control-Allow-Credentials', true);
-  //     return res.status(200).json({});
-  //   }
-  //   next();
-  // })
+  app.use("*", cors());
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
   app.use(express.json());
-
   app.use(cookiParser());
   app.use(authJwt());
   app.use(errorHandler);
