60e7e244201cff2fb08e27b65e3e1079df8500c6
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-06-02 20:23:47 +0200

    test login/form

--- client/real-estate/src/app/user/login/login.component.spec.ts
+++ client/real-estate/src/app/user/login/login.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +11,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule],
+      declarations: [LoginComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +25,28 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should check submit button(disabled)', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('#submit-btn');
+    expect(button.disabled).toBeTrue();
+  });
+
+  it('should check submit button(active)', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('#submit-btn');
+    component.loginFormGroup.get('email')?.setValue('user@email.com');
+    component.loginFormGroup.get('password')?.setValue('12345');
+
+    fixture.detectChanges();
+
+    expect(button.disabled).toBeFalsy();
+  });
+
+  it('shouldz test checkTouch method', () => {
+    component.loginFormGroup.controls['email'].markAsTouched();
+
+    fixture.detectChanges();
+
+    expect(component.checkTouch('email', component.loginFormGroup)).toBeTrue();
   });
 });
--- client/real-estate/src/app/user/login/login.component.unit.spec.ts
+++ client/real-estate/src/app/user/login/login.component.unit.spec.ts
@@ -3,18 +3,52 @@ import { FormBuilder } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 
 describe('Login form controller', () => {
-    let component: LoginComponent;
-    let userService: UserService;
+  let component: LoginComponent;
+  let userService: UserService;
 
-    beforeEach(() => {
-        component = new LoginComponent(new FormBuilder(), userService);
-    });
+  beforeEach(() => {
+    component = new LoginComponent(new FormBuilder(), userService);
+  });
 
-    it('should create a form with 2 controls', () => {
-        expect(component.loginFormGroup.contains('email')).toBeTruthy();
-        expect(component.loginFormGroup.contains('password')).toBeTruthy();
-    })
-      it('should test form controller(validity)', () => {
-        expect(component.loginFormGroup.valid).toBeFalsy();
-      });
-})
+  it('should create a form with 2 controls', () => {
+    expect(component.loginFormGroup.contains('email')).toBeTruthy();
+    expect(component.loginFormGroup.contains('password')).toBeTruthy();
+  });
+
+  it('should test form controller(validity)', () => {
+    expect(component.loginFormGroup.valid).toBeFalsy();
+  });
+    
+  //Unhappy path
+  it('should make email control required', () => {
+    let control = component.loginFormGroup.get('email');
+
+    control?.setValue('');
+
+    expect(control?.valid).toBeFalsy();
+  });
+
+  it('should make password control required', () => {
+    let control = component.loginFormGroup.get('password');
+
+    control?.setValue('');
+
+    expect(control?.valid).toBeFalsy();
+  });
+  //Happy path
+  it('shoudl make email control pass', () => {
+    let control = component.loginFormGroup.get('email');
+
+    control?.setValue('user@email.com');
+
+    expect(control?.valid).toBeTruthy();
+  });
+    
+  it('shoudl make email control pass', () => {
+    let control = component.loginFormGroup.get('password');
+
+    control?.setValue('12345');
+
+    expect(control?.valid).toBeTruthy();
+  });
+});
