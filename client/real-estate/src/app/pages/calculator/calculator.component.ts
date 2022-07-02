2ff86f5818706a0e8a4f6da3088f37a8d11a3118
Pullback993 <turgay.durhanov.ismailov@gmail.com>
2022-07-02 22:41:25 +0200

    html css 80% ready

--- client/real-estate/src/app/pages/calculator/calculator.component.ts
+++ client/real-estate/src/app/pages/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
+  // @ViewChild('contentWrapper') contentWrapper: ElementRef;
+  propTransferTax: number = 3.5;
+  allowEdit: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onDblClick($event: MouseEvent): void {
+    this.allowEdit = !this.allowEdit;
+    let currentNumber = Number((<HTMLTextAreaElement>$event.target).firstChild?.textContent);
+    this.propTransferTax = currentNumber;
   }
 
+  onBlur($event: FocusEvent) {
+    let pattern = /(\d.\d*)?(\d+)/gm;
+    this.allowEdit = !this.allowEdit;
+
+    let currentValue = (<HTMLTextAreaElement>$event.target).firstChild?.textContent;
+    try {
+      let num = pattern.exec(currentValue!)![0];
+      this.propTransferTax = Number(num);
+      //set space to avoid blur to close after right arrow push
+      (<HTMLTextAreaElement>$event.target).textContent = this.propTransferTax.toString() + ' ';
+    } catch (err) {
+      this.propTransferTax = 0;
+      (<HTMLTextAreaElement>$event.target).textContent = '0 ';
+    }
+  }
+
+  onKeyPress($event: KeyboardEvent) {
+    let totalLength = (<HTMLTextAreaElement>$event.target).firstChild?.textContent?.length || 0;
+
+    if (/[0-9]\.[0-9]|[0-9]+|\./gm.test($event.key) == false || totalLength >= 3) {
+      $event.preventDefault();
+    }
+  }
 }
